ecedfe086d85d6727a1e5bace04c90a8d3a125f3
unknown <dpinsky19@gmail.com>
2023-10-11 08:15:48 -0400

    finally done with this

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # leaflet-challenge
+
+This web app is a light-weight, no-frills app that allows a user to view earthquake data in the United States for a week earlier this year. 
+
+The fetch performed is from USGS.gov. 
+
+The magnitude and depth of the earthquake are listed in marker size and color respectively. 
+
+
+
+
+This was the first project I used ChatGPT on. It's a great tool to help with starting code.
--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,12 +1,14 @@
-  // Load earthquake data from the USGS GeoJSON feed (replace with your dataset URL)
+// Load earthquake data from the USGS GeoJSON feed
 fetch('https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2023-09-21%2000:00:00&endtime=2023-09-28%2023:59:59&maxlatitude=50&minlatitude=24.6&maxlongitude=-65&minlongitude=-125&minmagnitude=0.5&orderby=time')
       .then(response => response.json())
       .then(data => {
-          // Loop through the earthquake data and create markers
+          
+        // Loop through the earthquake data and create markers
           data.features.forEach(feature => {
               var coordinates = feature.geometry.coordinates;
               var magnitude = feature.properties.mag;
               var depth = coordinates[2];
+              
               // Create a marker with popup
               var marker = L.circleMarker([coordinates[1], coordinates[0]], {
                   radius: (magnitude * 2),
@@ -16,21 +18,20 @@ fetch('https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2023-0
                   opacity: 1,
                   fillOpacity: 0.8
               }).bindPopup(`<strong>Magnitude:</strong> ${magnitude}<br><strong>Depth:</strong> ${depth} km`);
-              // Add the marker to the map
               marker.addTo(map);
           });
       });
-  // Create a map centered at a specific location and with an initial zoom level
+  
+      // Create a map centered at a specific location and with an initial zoom level
   var map = L.map('map').setView([0, 0], 2);
+  
   // Add a tile layer for the base map (you can choose a different one)
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
   }).addTo(map);
-  // Define a function to determine marker size based on earthquake magnitude
-  //function getMarkerSize(magnitude) {
-   //   return ((magnitude)) * 5;
-  //}
+
   // Define a function to determine marker color based on earthquake depth
+    //  Colors listed in general order of severity
   function getMarkerColor(depth) {
       if (depth < 10) {
           return 'green';
@@ -44,7 +45,8 @@ fetch('https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2023-0
           return 'darkred';
       }
   }
-  // Create a legend
+
+  // Create legend
   var legend = L.control({ position: 'bottomright' });
   legend.onAdd = function () {
       var div = L.DomUtil.create('div', 'legend');
